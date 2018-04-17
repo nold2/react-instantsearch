57c546ccf059a640949966df94a75b79fcefc8d6
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-04-17 12:33:47 +0200

    WIP

--- stories/GeoSearch.stories.js
+++ stories/GeoSearch.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { setAddon, storiesOf } from '@storybook/react';
-import { GeoSearch } from '../packages/react-instantsearch/dom';
+import { GeoSearch, Configure } from '../packages/react-instantsearch/dom';
 import { displayName, filterProps, WrapWithHits } from './util';
 import JSXAddon from 'storybook-addon-jsx';
 
@@ -8,19 +8,23 @@ setAddon(JSXAddon);
 
 const stories = storiesOf('GeoSearch', module);
 
+const position = '37.7793, -122.419';
+const radius = 5000;
+const precision = 2500;
+
+// Default
 stories.addWithJSX(
   'default',
   () => (
     <WrapWithHits
       linkedStoryGroup="GeoSearch"
       indexName="airbnb"
-      searchBox={false}
       searchParameters={{
         hitsPerPage: 25,
-        aroundLatLngViaIP: true,
-        aroundRadius: 10000,
       }}
     >
+      <Configure aroundLatLngViaIP />
+
       <GeoSearch
         googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
         loadingElement={<div style={{ height: `100%` }} />}
@@ -34,3 +38,284 @@ stories.addWithJSX(
     filterProps,
   }
 );
+
+// With IP
+stories
+  .addWithJSX(
+    'with IP',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLngViaIP />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'with IP & radius',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLngViaIP aroundRadius={radius} />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'with IP & radius & precision',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure
+          aroundLatLngViaIP
+          aroundRadius={radius}
+          aroundPrecision={precision}
+        />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  );
+
+// With Position
+stories
+  .addWithJSX(
+    'with position',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLng={position} />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'with position & radius',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLng={position} aroundRadius={radius} />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'with position & radius & precision',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure
+          aroundLatLng={position}
+          aroundRadius={radius}
+          aroundPrecision={precision}
+        />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  );
+
+// With Places @TODO
+stories.addWithJSX('with Places', () => <div>TODO</div>, {
+  displayName,
+  filterProps,
+});
+
+// Only UI
+stories
+  .addWithJSX(
+    'with control & refine on map move',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLngViaIP />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          enableRefineControl
+          enableRefineOnMapMove
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'with control & disable refine on map move',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLngViaIP />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          enableRefineOnMapMove={false}
+          enableRefineControl
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'without control & refine on map move',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLngViaIP />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          enableRefineControl={false}
+          enableRefineOnMapMove
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  )
+  .addWithJSX(
+    'without control & disable refine on map move',
+    () => (
+      <WrapWithHits
+        linkedStoryGroup="GeoSearch"
+        indexName="airbnb"
+        searchParameters={{
+          hitsPerPage: 25,
+        }}
+      >
+        <Configure aroundLatLngViaIP />
+
+        <GeoSearch
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyCl2TTJXpwxGuuc2zQZkAlIkWhpYbyjjP8"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `500px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+          enableRefineControl={false}
+          enableRefineOnMapMove={false}
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
+  );
